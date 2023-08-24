aade4333cceac311d752caec237759a7782e740f
rhdehgks <rhdehgks@naver.com>
2023-08-25 02:08:25 +0900

    클라이언트 간 통신 추가

--- server.py
+++ server.py
@@ -1,13 +1,23 @@
 import asyncio;
 import websockets;
  
-async def server(ws, path):
-    async for msg in ws:
-        msg = msg.decode("utf-8")
-        print(f"Msg from client: {msg}")
-        await ws.send(f"{msg}")
+connected_clients = set()
 
-start_server = websockets.serve(server, "13.125.159.144", 5000)
+async def server(ws, path):
+    connected_clients.add(ws)
+    try:
+        async for msg in ws:
+            msg = msg.decode("utf-8")
+            print(f"Msg from client: {msg}")
+            
+            for client in list(connected_clients):
+                print(client, ws)
+                if client is not ws:
+                    await client.send(msg)
+    finally:
+        connected_clients.remove(ws)
+
+start_server = websockets.serve(server, "0.0.0.0", 5000)
 print("Server started")
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
