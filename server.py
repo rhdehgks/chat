5b4c025a21c9afe90a6bb29b47985e98f11d1cad
rhdehgks <rhdehgks@naver.com>
2023-08-25 00:46:25 +0900

    첫 커밋

---
+++ server.py
@@ -0,0 +1,13 @@
+import asyncio;
+import websockets;
+ 
+async def server(ws, path):
+    async for msg in ws:
+        msg = msg.decode("utf-8")
+        print(f"Msg from client: {msg}")
+        await ws.send(f"{msg}")
+
+start_server = websockets.serve(server, "localhost", 5000)
+print("Server started")
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
