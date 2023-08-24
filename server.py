a3d2bb78862153d9dcb8e9d56a341f16d3e40b97
rhdehgks <rhdehgks@naver.com>
2023-08-25 02:46:05 +0900

    채팅 형식 수정

--- server.py
+++ server.py
@@ -1,20 +1,28 @@
 import asyncio;
 import websockets;
+import random;
  
 connected_clients = set()
+words = {
+    '나그네', '거북이', '사람', '누구게', '아무개', '홍길동', '외계인'
+}
+names = dict()
 
 async def server(ws, path):
     connected_clients.add(ws)
+    names[ws] = words.pop()
+    for client in list(connected_clients):
+        await client.send(f"{names[ws]}님이 입장하였습니다.")
     try:
         async for msg in ws:
             msg = msg.decode("utf-8")
             print(f"Msg from client: {msg}")
-            
             for client in list(connected_clients):
-                print(client, ws)
                 if client is not ws:
-                    await client.send(msg)
+                    await client.send(f"{names[ws]}: {msg}")
     finally:
+        words.add(names[ws])
+        del names[ws]
         connected_clients.remove(ws)
 
 start_server = websockets.serve(server, "0.0.0.0", 5000)
